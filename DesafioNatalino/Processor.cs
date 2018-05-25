029c509a89702ebfb69b7ca6d941fc1c72d2d75e
Thaise Dantas <tdantas@stone.com.br>
2018-05-25 12:30:12 -0300

    test

--- DesafioNatalino/Processor.cs
+++ DesafioNatalino/Processor.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -12,7 +13,7 @@ namespace DesafioNatalino
             {
                 HostName = "localhost",
                 UserName = "guest",
-                Password = "guest", 
+                Password = "guest",
                 VirtualHost = "/"
             };
 
@@ -20,19 +21,86 @@ namespace DesafioNatalino
             var channel = connection.CreateModel();
 
             channel.QueueDeclare("hello", false, false, false, null);
-            
+
             channel.ExchangeDeclare("exchangeTest", "fanout", true);
 
             channel.QueueBind("hello", "exchangeTest", "rota1");
 
             var body = Encoding.UTF8.GetBytes(message.Message);
-            channel.BasicPublish("exchangeTest", "rota1",  null,  body);
+            channel.BasicPublish("exchangeTest", "rota1", null, body);
 
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
-                
+
             };
         }
+
+
+
+        public void Emit(string args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "logs",
+                    routingKey: "",
+                    basicProperties: null,
+                    body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+
+        }
+
+        public void Receive()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                    exchange: "logs",
+                    routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+
+
+        private static string GetMessage(string args)
+        {
+            return ((args.Length > 0)
+                ? string.Join(" ", args)
+                : "info: Hello World!");
+        }
+
+
     }
 }
